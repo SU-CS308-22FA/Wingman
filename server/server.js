57e7fbd91bec447c304f970c28abb74dd7880977
Ahmet Furkan Un <fun@sabanciuniv.edu>
2022-11-02 00:26:41 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import wingman from "./api/wingman.route.js";
 import express from "express";
 import cors from "cors";
+import path from "path"
+import {fileURLToPath} from 'url';
 
 const app = express()
 
@@ -8,7 +10,8 @@ app.use(cors())
 app.use(express.json()) //For funcionality to read json
 
 app.use("/api/Wingman", wingman) 
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 // After defining your routes, anything that doesn't match what's above, we want to return index.html from our built React app
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../client/build/index.html'))
