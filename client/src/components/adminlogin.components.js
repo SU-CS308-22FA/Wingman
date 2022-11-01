aa44bcce9d6954aa2403f499d57da4f1855df4c0
Ege Demirci <52569205+egedemirci@users.noreply.github.com>
2022-11-01 13:46:44 +0300

    Login Component

---
+++ client/src/components/adminlogin.components.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ResponsiveAppBar from './WelcomeWingmanBar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#A99985',
+    },
+    secondary: {
+      main: '#70798C',
+    },
+  },
+});
+
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Wingman
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+const Login = ({}) => {
+    const [inputs, setInputs] = useState({
+      email: "",
+      password: ""
+    });
+  
+    const { email, password } = inputs;
+  
+    const onChange = e =>
+      setInputs({ ...inputs, [e.target.name]: e.target.value });
+  
+    const onSubmitForm = async e => {
+      e.preventDefault();
+      try {
+        const body = { email, password };
+        const response = await fetch(
+          "http://localhost:5000/authentication/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify(body)
+          }
+        );
+    }
+
+    catch(err){}
+    
+    };
+  
+
+  return (
+    <ThemeProvider theme={theme}>
+            <ResponsiveAppBar/>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+           <img src="https://i.hizliresim.com/t6q9rs6.png" height="66" width="50" />          
+           <Box m={1} pt={0}> </Box>
+
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={onSubmitForm} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+export default Login;
