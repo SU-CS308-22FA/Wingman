e7c22a6eefb4d6cb7c5b53d0b7890e20d32e0d2e
DenizGezgin <denizgzgn@gmail.com>
2022-11-19 14:59:08 +0300

    superAdminAdded

---
+++ client/src/routes/SuperAdmin/SuperAdminMain.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import ResponsiveAppBar from '../../components/WelcomeWingmanBar';
+
+
+const SuperAdminMain = () => {
+    return (
+      <div>
+        <ResponsiveAppBar/>
+        < SuperAdminList/>
+      </div>
+    );
+  };
+   
+  export default SuperAdminMain;
