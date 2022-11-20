26ecd1f0f6f229b02f7929f0c18cee82a93df894
Ahmet Furkan Un <fun@sabanciuniv.edu>
2022-11-20 22:08:00 +0300

    Referee profile remember me and auth implemented

--- client/src/routes/RR/RRRefereeProfile.jsx
+++ client/src/routes/RR/RRRefereeProfile.jsx
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Copyright from "../../components/Copyright.component";
-import ResponsiveAppBar from '../../components/WelcomeWingmanBar';
 import RefereeProfile  from "../../components/RefereeProfile.component";
+import { useNavigate } from "react-router-dom";
+import UserFinder from "../../apis/UserFinder";
+import { UsersContext } from "../../context/UserContex";
+import { AuthContext } from "../../context/authContext";
+import { useState } from "react";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import RefAppBar from "../../components/RetiredRefReporterAppBar";
+import ResponsiveAppBar from "../../components/LoggedInAppBar";
 
 const RRRefereeProfile = () => {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(true)
+  const {user, setUser} = useContext(UsersContext)
+  const {setAuth} = useContext(AuthContext)
+
+  const getData = async () => {
+    try {
+      await UserFinder.get(`/users/1`, {headers: {'jwt_token': localStorage.token}})
+      .then(userData =>{
+        let val = {
+          id: userData.data.data.user_id,
+          mail: userData.data.data.mail,
+          name: userData.data.data.name,
+          surname: userData.data.data.surname,
+          role: userData.data.data.role,
+        }
+        setUser(val)
+      })
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const checkAuthenticated = async () => {
+    try {
+      const res = await UserFinder.post("/verify", {}, {headers: {'jwt_token': localStorage.token}})
+      if (res.data.isAuth === true){
+        await getData();
+        await setAuth(true);
+        setLoading(false)
+      }
+      else{
+        await setAuth(false);
+        navigate("/")
+      } 
+    } catch (err) {
+      console.error(err.message);
+      await setAuth(false);
+      navigate("/")
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+    if(isLoading){
+      return (
+        <div>
+          <Box m={0} pt={34}> </Box>
+          <center> <CircularProgress /></center>
+        </div>
+      );
+    }
+    else if(user.role == "TFF Admin")
+    {
     return (
       <div>
         <ResponsiveAppBar/>
@@ -11,6 +74,17 @@ const RRRefereeProfile = () => {
         <Copyright sx={{ mt: 5 }} />
       </div>
     );
+    }
+    else if(user.role == "Reporter" || user.role=="Retired Referee")
+    {
+    return (
+      <div>
+        <RefAppBar/>
+        <RefereeProfile />
+        <Copyright sx={{ mt: 5 }} />
+      </div>
+    );
+    }
   };
 
   export default RRRefereeProfile;
