1addaf4380e446d05d289b075e708b0561cc49ce
Ege Demirci <52569205+egedemirci@users.noreply.github.com>
2022-12-03 21:50:03 +0300

    Team Dashboard Started

--- client/src/components/TeamProfile.component.jsx
+++ client/src/components/TeamProfile.component.jsx
@@ -9,60 +9,66 @@ import UserFinder from "../apis/UserFinder";
 import { Avatar, Chip, Divider } from '@mui/material';
 
 export const TeamProfile = () => {
-    const [team, setTeam] = useState({});
+  const [team, setTeam] = useState(null);
+  
+  const queryString = window.location.href;
+  const id = queryString.match(/teams\/(\d+)/)[1];
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await UserFinder.get(`/teams/${id}`);
+        if (response.data.data.data) {
+          setTeam(prevTeam => response.data.data.data);
+        } else {
+          setTeam(prevTeam => []);
+        }
+      } catch (err) {
+        setTeam(prevTeam => []);
+      }
+    }
+  
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
 
-    const queryString = window.location.href;
-    const index = queryString.search("teams")
-    const id = queryString.substring(index + 6)
-    
-    useEffect( () => {
-        try {
-            UserFinder.get(`/teams/${id}`)
-            .then(response =>{
-            setTeam(response.data.data)
-            console.log(team);}
-            );
-        }catch (err) {}
-    }, [])
+  return (
+    team !== null ? (
+      <div>
+        <main>
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="md">
+              <Typography
+                variant="h3"
+                align="center"
+                color="#70798C"
+                gutterBottom
+              >
+                {team.length}
+                {team[9].teamname}
 
-    return (
-    
-        <div>
-            <main>
-            <Box
-              sx={{
-                bgcolor: 'background.paper',
-                pt: 8,
-                pb: 6,
-              }}
-            >
-
-              <Container maxWidth="md">
-                <Typography
-                  variant="h3"
-                  align="center"
-                  color="#70798C"
-                  gutterBottom
-                >
-                   {team.teamname} 
-                </Typography>
-                <Typography
-                  variant="h5"
-                  align="center"
-                  color="#70798C"
-                  gutterBottom
-                >   
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="#70798C"
+                gutterBottom
+              >
                 This profile with full details will be implemented in the next sprint.
-                </Typography>
-                
-              </Container>
-            </Box>
-    
-          </main>
+              </Typography>
+            </Container>
+          </Box>
+        </main>
       </div>
-        
-        
-    )
+    ) : null
+  )
 }
 
 export default TeamProfile;
