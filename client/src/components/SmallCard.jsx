9d294bfcdf67f079bdfe82058e020868204d00ce
Ege Demirci <52569205+egedemirci@users.noreply.github.com>
2022-12-05 20:15:24 +0300

    Team Dashboard Completed

--- client/src/components/SmallCard.jsx
+++ client/src/components/SmallCard.jsx
@@ -7,17 +7,16 @@ import MainCard from './MainCard';
 // project import
 // assets
 
-// ==============================|| STATISTICS - ECOMMERCE CARD  ||============================== //
 
 const SmallCard = ({ title, subtitle,value}) => (
     <MainCard contentSX={{ p: 2.25 }}>
         <Stack spacing={0.5}>
-            <Typography variant="h6" color="textSecondary">
+            <Typography variant="h6" color="#252323" font fontWeight="bold">
                 {title}
             </Typography>
             <Grid container alignItems="center">
                 <Grid item>
-                    <Typography variant="h4" color="inherit">
+                    <Typography variant="h5" color="#A99985" font fontWeight="light">
                         {subtitle}
                     </Typography>
                 </Grid>
@@ -25,7 +24,7 @@ const SmallCard = ({ title, subtitle,value}) => (
                 <Grid item>
                 <Chip
                     label={(value < 65 ? "Fair" : value <= 80 ? "Mediocre" : "Severe")}
-                    color={value <= 80 ? "secondary" : value < 65 ? "success" : "error"}
+                    color={ value < 65 ? "success" : value <= 80 ? "secondary" : "error"}
                     sx={{ ml: 1.25, mb:1, pl: 0 }}
                     size="small"
                 />
