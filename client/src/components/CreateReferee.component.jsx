03264c4ce637addd4b16c77b47067ebb2d379963
Ege Demirci <52569205+egedemirci@users.noreply.github.com>
2022-12-06 23:59:27 +0300

    Add referee creation page
    This commit adds a new page for creating referees in the TFF database.
    The page includes form fields for entering the referee's name, surname,
    and other stats like red card, yellow card, matches. This new page is
    accessible from the referee list and is properly linked to the rest of
    the application.

--- client/src/components/CreateReferee.component.jsx
+++ client/src/components/CreateReferee.component.jsx
@@ -16,6 +16,7 @@ const CreateReferee = () => {
   const navigate = useNavigate();
   const [referee, setReferee] = useState(null);
   const [error, setError] = useState(null)
+ 
   
   const [inputs, setInputs] = useState({
     name: "",
@@ -33,19 +34,26 @@ const CreateReferee = () => {
     currentpenpg: 0,
 
   });  
+  
+ 
 
   const { name, surname, totalmatches,totalyellowcards, totalredcards, age, currentseasonmatches,
     totalfoulspg,currentyel,currentfoulspg,currentred,totalpenpg,currentpenpg} = inputs;
     function isAlphabetical(s) {
         // Use a regex to match only alphabetical characters and spaces
         const pattern = /^[a-zA-Z ]*$/;
-      
         // Check if the string matches the pattern
         if (pattern.test(s)) {
           return true;
         } else {
           return false;
         }
+      }
+    function isNumeric(value) {
+        return !isNaN(value);
+      }
+      function isInteger(value) {
+        return /^-?\d+$/.test(value);
       }
 
   function isValidPass(pass) {
@@ -56,20 +64,35 @@ const CreateReferee = () => {
       return false;
     }
   };
-
-  const onChange = e =>
+   const onChange = e =>
     setInputs({ ...inputs, [e.target.name]: e.target.value });
 
   const onSubmitForm = async e => {
     e.preventDefault();
     try {
+      if(!isAlphabetical(name)){
+        throw{fmessage: "Name contains non-alphabetical characters. Please check the name field."} }    
+      else if(!isAlphabetical(surname)){
+        throw{fmessage: "Surname contains non-alphabetical characters. Please check the name field."}}
+      else if(!isInteger(age) || !isInteger(totalmatches) || !isInteger(totalredcards) || !isInteger(totalyellowcards) ||
+              !isInteger(currentseasonmatches)|| !isInteger(currentyel)||  !isInteger(currentred)){
+        throw{fmessage: "Total exact amounts cannot be empty and all of them must be integers."}}
+      else if(!isNumeric(totalfoulspg)|| !isNumeric(currentfoulspg) || !isNumeric(totalpenpg)|| !isNumeric(currentpenpg)){
+        throw{fmessage: "Statistical values cannot include non numeric values."}}
+      else if(totalmatches.length != 0 && currentseasonmatches.length != 0 && totalmatches<currentseasonmatches){
+                throw{fmessage: "Total number of matches cannot be smaller than current season matches. Please check the both fields."}}
+      else if(totalredcards.length != 0 && currentred.length != 0 && totalredcards<currentred){
+        console.log(currentred);
 
-      if(!isAlphabetical(name))
-          throw{fmessage: "Name contains non-alphabetical characters. Please check the name field."}     
-        if(!isAlphabetical(surname))
-            throw{fmessage: "Surname contains non-alphabetical characters. Please check the name field."}
-        if(name.length == 0 || surname.length == 0 || age.length == 0)
-            throw{fmessage: "Please fill all the fields."}
+                  throw{fmessage: "Total number of red cards cannot be smaller than current season red cards. Please check the both fields."}}
+      else if(totalyellowcards.length != 0 && currentyel.length != 0 && totalyellowcards<currentyel){
+                    throw{fmessage: "Total number of yellow cards cannot be smaller than current season yellow cards. Please check the both fields."}}
+      else if(name.length == 0 || surname.length == 0 || age.length == 0 || totalmatches== 0 || totalyellowcards== 0|| totalredcards== 0 || currentseasonmatches== 0||
+                            totalfoulspg== 0||currentyel== 0||currentfoulspg== 0||currentred== 0||totalpenpg== 0||currentpenpg== 0){
+          throw{fmessage: "All fields must be filled. Please check if there any empty field."}}
+
+
+
 
 
 
@@ -113,26 +136,22 @@ const CreateReferee = () => {
             avatarurl:response.data.data.avatarurl
           } 
           setReferee(val)  
-          navigate("/profile/")}
+          navigate("/refereelist")}
     } catch (err) {
         if(err.fmessage)
             setError(err.fmessage)
-        else if(err.response.status == 401)
-            setError("Selected mail is already in use, please change your mail.")
-        else if(err.response.status == 402)
-            setError("Invalid security key. Please make sure you are using a correct key that matches your role.")
         else
-            setError("There was an unknown problem")
+            setError("There was an unknown problem.")
         console.error('onSubmit form error: ', err);
     }
   };
 
   return (
-      <Container component="main" maxWidth="xs">
+      <Container component="main">
         <CssBaseline />
-        <Box
+        <Box margin={{ left: "auto", right: "auto" }}
           sx={{
-            marginTop: 8,
+            marginTop: 6,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
@@ -140,8 +159,11 @@ const CreateReferee = () => {
         >
                   
           <Box m={1} pt={0}> </Box>
-          { <Typography component="h3" variant="h5">
-                     Create Referee </Typography>  }
+          <Typography variant="h4" color="#A99985" font fontWeight="600">
+                Create Referee
+            </Typography>
+            <Typography variant="h6" color="#70798C" font fontWeight="200">
+            </Typography>
           
 
           <Box component="form" noValidate onSubmit={onSubmitForm} sx={{ mt: 3 }}>
@@ -169,8 +191,19 @@ const CreateReferee = () => {
                   onChange={e => onChange(e)}
                 />
               </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="totalmatches"
+                  label="Total Matches"
+                  name="totalmatches"
+                  autoComplete={currentseasonmatches}
+                  onChange={e => onChange(e)}
+                />
+              </Grid>
 
-                <Grid item xs={12}>
+              <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
@@ -179,8 +212,113 @@ const CreateReferee = () => {
                   name="age"
                   autoComplete="age"
                   onChange={e => onChange(e)}
+                  
                 />
               </Grid>
+
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  required
+                  fullWidth
+                  id="totalredcards"
+                  label="Total Red Cards"
+                  name="totalredcards"
+                  autoComplete="totalredcards"
+                  onChange={e => onChange(e)}
+                />
+              </Grid>
+    
+
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="totalyellowcards"
+                 label="Total Yellow Cards"
+                  name="totalyellowcards"
+                 autoComplete="totalyellowcards"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentseasonmatches"
+                 label="Current Season Matches"
+                  name="currentseasonmatches"
+                 autoComplete="currentseasonmatches"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="totalfoulspg"
+                 label="Total Fouls Per Game"
+                  name="totalfoulspg"
+                 autoComplete="totalfoulspg"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentyel"
+                 label="Current Season Yellow Cards"
+                  name="currentyel"
+                 autoComplete="currentyel"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentfoulspg"
+                 label="Current Fouls Per Game"
+                  name="currentfoulspg"
+                 autoComplete="currentfoulspg"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentred"
+                 label="Current Season Red Cards"
+                  name="currentred"
+                 autoComplete="currentred"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="totalpenpg"
+                 label="Total Penalty Per Game"
+                  name="totalpenpg"
+                 autoComplete="totalpenpg"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+<Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentpenpg"
+                 label="Current Penalty Per Game"
+                  name="currentpenpg"
+                 autoComplete="currentpenpg"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+
             </Grid>
             <Box m={1} pt={0}> </Box>
             {error &&<Alert variant="filled" severity="error"> {error} </Alert>}
@@ -193,15 +331,6 @@ const CreateReferee = () => {
             >
               Create Referee
             </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-               <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link> 
-                
-                
-              </Grid>
-            </Grid>
           </Box>
         </Box>
       </Container>  );
