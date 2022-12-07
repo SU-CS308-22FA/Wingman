46d00255ee10a5b42dade66bced1794b28c1c813
DenizGezgin <denizgzgn@gmail.com>
2022-12-07 21:50:41 +0300

    finshed match page

--- client/src/components/RatingComponents/ReporterMatch.component.jsx
+++ client/src/components/RatingComponents/ReporterMatch.component.jsx
@@ -1,9 +1,12 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
+import { UsersContext } from "../../context/UserContex";
 import Typography from "@mui/material/Typography";
 import { Avatar, Box, Grid, Paper } from "@mui/material";
 import { Container } from "@mui/system";
@@ -19,36 +22,34 @@ import LaptopMacIcon from "@mui/icons-material/LaptopMac";
 import HotelIcon from "@mui/icons-material/Hotel";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import SmallCard from "../SmallCard";
+import Rating from '@mui/material/Rating';
+import StarIcon from '@mui/icons-material/Star';
 import CardReasonsCard from "./CardReasonCard.component";
 import RedYellowCardCard from "./RedYellowCardCard.component";
 import FoulCard from "./FoulCard.component";
 import HoverRating from "./Rating.component";
 import SendIcon from "@mui/icons-material/Send";
+import UserFinder from "../../apis/UserFinder.js";
 
-let timelineList = [
-  {
-    text: "Berk Akkol",
-    matchTime: "12",
-    eventType: "Yellow Card",
-    eventSide: "Away",
-  },
-  { text: "Gol adam", matchTime: "23", eventType: "Goal", eventSide: "Home" },
-  { text: "Gol adam", matchTime: "23", eventType: "Goal", eventSide: "Home" },
-  { text: "Gol adam", matchTime: "23", eventType: "Goal", eventSide: "Home" },
-  { text: "Gol adam", matchTime: "23", eventType: "Goal", eventSide: "Home" },
-  {
-    text: "Berk Akkol",
-    matchTime: "90+2",
-    eventType: "Red Card",
-    eventSide: "Away",
-  },
-  {
-    text: "Anil Koyuncu with Cemal Yilmaz",
-    matchTime: "90+13",
-    eventType: "Change",
-    eventSide: "Home",
-  },
-];
+let timeline = [];
+
+const labels = {
+  1: 'Very poor',
+  2: 'Very poor+',
+  3: 'Poor',
+  4: 'Poor+',
+  5: 'Ok',
+  6: 'Ok+',
+  7: 'Good',
+  8: 'Good+',
+  9: 'Excellent',
+  10: 'Excellent+',
+};
+
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+}
+
 
 function createTimelineItem(text, eventSide, matchTime, eventType) {
   let homeText;
@@ -106,223 +107,342 @@ function getIconSrcFromType(eventType) {
 }
 
 export default function ReporterMatch() {
-  return (
-    <Grid
-      direction="row"
+  const [matchData, setMatchData] = useState();
+  const [isLoading, setLoading] = useState(true);
+  const [isAvalible, setAvalible] = useState(true);
+  const [hasRated, setRated] = useState(false);
+  const {user} = useContext(UsersContext)
+  const [value, setValue] = React.useState(5);
+  const [hover, setHover] = React.useState(5);
+
+
+  const navigate = useNavigate();
+
+  let id;
+
+  const fetcData = async () => {
+    try {
+      const response = await UserFinder.get(`/match/${id}`);
+      setMatchData(response.data.data);
+      timeline = (response.data.timeline);
+      setLoading(false);
+    } catch (err) {
+      setAvalible(false);
+    }
+  };
+
+  function renderRate(){
+    if(user.role == "TFF Admin")
+    {
+      return(<CardContent><Box display="flex"
       justifyContent="center"
-      alignItems="center"
-      container
-      spacing={1}
-    >
-      <Grid>
-        <Card
-          sx={{ height: 200, width: 1200, mt: 4 }}
-          style={{ backgroundColor: "#DAD2BC" }}
+      alignItems="center">Admins can't rate.</Box></CardContent>)
+    }
+    else if(hasRated)
+    {
+      return(<CardContent><Box display="flex"
+      justifyContent="center"
+      alignItems="center">Your rate submitted.</Box></CardContent>)
+    }
+    else
+    {
+      return(<CardContent>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-around"
+          alignItems="center"
         >
-          <CardContent>
+          <Grid item>
+            {" "}
             <Grid
+              sx={{ mt: 0.4 }}
               container
-              direction="row"
-              justifyContent="space-around"
+              spacing={2}
+              direction="column"
+              justifyContent="center"
               alignItems="center"
-              sx={{ mt: 2 }}
             >
-              <Grid item xs={2}>
-                <Typography sx={{ ml: 3 }}>HOME</Typography>
+              <Grid item>
                 <Avatar
                   alt="Remy Sharp"
-                  src="https://upload.wikimedia.org/wikipedia/tr/8/86/Fenerbah%C3%A7e_SK.png?20221115185307"
-                  sx={{ width: 100, height: 100, mt: 1 }}
+                  src={matchData.avatarurl}
+                  sx={{ width: 100, height: 100 }}
                 />
-                <Typography sx={{ ml: 1.1 }}>Fenerbahçe</Typography>
               </Grid>
-              <Grid
-                item
-                xs={2}
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Grid>
-                  <Typography
-                    color="#A99985"
-                    style={{ fontWeight: "bold", fontSize: 75 }}
-                  >
-                    1
-                  </Typography>
-                </Grid>
-                <Grid>
-                  <Typography color="#A99985" style={{ fontSize: 75 }}>
-                    :
-                  </Typography>
-                </Grid>
-                <Grid>
-                  <Typography
-                    sx={{ mr: 10 }}
-                    color="#A99985"
-                    style={{ fontWeight: "bold", fontSize: 75 }}
-                  >
-                    2
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid item xs={1.2}>
-                <Typography sx={{ ml: 3 }}>AWAY</Typography>
-                <Avatar
-                  alt="Remy Sharp"
-                  src="https://upload.wikimedia.org/wikipedia/tr/2/22/Bergama_Belediyespor.png?20110720181143"
-                  sx={{ width: 100, height: 100, mt: 1 }}
-                />
-                <Typography sx={{ width: 400 }}>
-                  Bergama Belediye Spor
+              <Grid item>
+                <Typography
+                  color="black"
+                  style={{ fontWeight: "bold", fontSize: 20 }}
+                >
+                  {matchData.name + " " + matchData.surname + ", " + matchData.age}
                 </Typography>
               </Grid>
             </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid
-        sx={{ mt: 4 }}
-        container
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Typography color="black" style={{ fontWeight: "bold", fontSize: 20 }}>
-          Match Starts
-        </Typography>
-      </Grid>
-      <Grid
-        container
-        direction="row"
-        justifyContent="space-between"
-        alignItems="flex-start"
-      >
-        <Grid item>
-          <Box sx={{ ml: 12 }}>
-            <Grid
-              container
-              spacing={4}
-              direction="column"
-              justifyContent="flex-start"
-              alignItems="flex-start"
-            >
-              <Grid item>
-                <CardReasonsCard totalCardsDist={[1, 2, 3, 4, 5]} />
-              </Grid>
-              <Grid item>
-                {" "}
-                <RedYellowCardCard numRed="10" numYellow="2" />{" "}
-              </Grid>
-              <Grid item>
-                <FoulCard numFoul="10" numFoulPerCard="20" />
-              </Grid>
-            </Grid>
-          </Box>
+          </Grid>
+          <Grid item>
+          <Grid container
+direction="column"
+justifyContent="center"
+alignItems="center">
+  <Grid item>
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={1}
+        size="large"
+        max={10}
+        defaultValue={5}
+        getLabelText={getLabelText}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+      />
+  </Grid>
+  <Grid item>{value !== null && (
+    <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+  )}</Grid>
+</Grid>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" endIcon={<SendIcon />} onClick={handleSubmit}>
+              Send
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Timeline sx={{ width: 450 }}>
-            {timelineList.map((item) => (
-              <div
-                key={
-                  item.text + item.matchTime + item.eventType + item.eventSide
-                }
-              >
-                {createTimelineItem(
-                  item.text,
-                  item.eventSide,
-                  item.matchTime,
-                  item.eventType
-                )}
-              </div>
-            ))}
-          </Timeline>
-        </Grid>
-        <Grid item>
-          <Box sx={{ mr: 12 }}>
-            <Grid
-              container
-              spacing={4}
-              direction="column"
-              justifyContent="flex-start"
-              alignItems="flex-start"
-            >
-              <Grid item>
-                <CardReasonsCard totalCardsDist={[1, 2, 3, 4, 5]} />
-              </Grid>
-              <Grid item>
-                {" "}
-                <RedYellowCardCard numRed="10" numYellow="2" />{" "}
-              </Grid>
-              <Grid item>
-                <FoulCard numFoul="10" numFoulPerCard="20" />
-              </Grid>
-            </Grid>
-          </Box>
-        </Grid>
-      </Grid>
-      <Grid
-        sx={{ mt: 0 }}
-        container
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Typography color="black" style={{ fontWeight: "bold", fontSize: 20 }}>
-          Match Ends
-        </Typography>
-      </Grid>
+      </CardContent>)
+    }
+  }
 
-      <Grid>
-        <Card
-          sx={{ height: 200, width: 800, mt: 4 }}
-          style={{ backgroundColor: "#DAD2BC" }}
+  function handleSubmit(){
+    setRated(true);
+    console.log(user)
+    console.log(value)
+  }
+
+  useEffect(() => {
+    const queryString = window.location.href;
+    id = queryString.match(/match\/(\d+)/)[1];
+    fetcData();
+  }, []);
+
+  if(!isAvalible)
+  {
+    return (<h1>Match not in db...</h1>);
+  }
+  else
+  {
+    if (!isLoading) {
+      return (
+        <Grid
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          container
+          spacing={1}
         >
-          <CardContent>
-            <Grid
-              container
-              direction="row"
-              justifyContent="space-around"
-              alignItems="center"
+          <Grid>
+            <Card
+              sx={{ height: 200, width: 1200, mt: 4 }}
+              style={{ backgroundColor: "#DAD2BC" }}
             >
-              <Grid item>
-                {" "}
+              <CardContent>
                 <Grid
-                  sx={{ mt: 0.4 }}
                   container
-                  spacing={2}
-                  direction="column"
-                  justifyContent="center"
+                  direction="row"
+                  justifyContent="space-around"
                   alignItems="center"
+                  sx={{ mt: 2 }}
                 >
-                  <Grid item>
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://www.nereli.org/img/hakem/yasar-kemal-ugurlu.jpg"
-                      sx={{ width: 100, height: 100 }}
-                    />
+                  <Grid item xs={2}>
+                    <Grid
+                    container
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center">
+                      <Grid item><Typography>HOME</Typography></Grid>
+                      <Grid item>
+                        <Avatar
+                          src={matchData.home_logo}
+                          sx={{ width: 100, height: 100}}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Typography>
+                          {matchData.home_teamname}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={2}
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Grid>
+                      <Typography
+                        color="#A99985"
+                        style={{ fontWeight: "bold", fontSize: 75 }}
+                      >
+                        {matchData.home_score}
+                      </Typography>
+                    </Grid>
+                    <Grid>
+                      <Typography color="#A99985" style={{ fontSize: 75 }}>
+                        :
+                      </Typography>
+                    </Grid>
+                    <Grid>
+                      <Typography
+                        sx={{ mr: 10 }}
+                        color="#A99985"
+                        style={{ fontWeight: "bold", fontSize: 75 }}
+                      >
+                        {matchData.away_score}
+                      </Typography>
+                    </Grid>
                   </Grid>
                   <Grid item>
-                    <Typography
-                      color="black"
-                      style={{ fontWeight: "bold", fontSize: 20 }}
-                    >
-                      Yasar Kemal Ugurlu
-                    </Typography>
+                  <Grid
+                    container
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center">
+                      <Grid item><Typography>AWAY</Typography></Grid>
+                      <Grid item>
+                        <Avatar
+                          src={matchData.away_logo}
+                          sx={{ width: 100, height: 100}}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Typography>
+                          {matchData.away_teamname}
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-              <Grid item>
-                <HoverRating />
-              </Grid>
-              <Grid item>
-                <Button variant="contained" endIcon={<SendIcon />}>
-                  Send
-                </Button>
-              </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid
+            sx={{ mt: 4 }}
+            container
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography
+              color="black"
+              style={{ fontWeight: "bold", fontSize: 20 }}
+            >
+              Match Starts
+            </Typography>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="flex-start"
+          >
+            <Grid item>
+              <Box sx={{ ml: 12 }}>
+                <Grid
+                  container
+                  spacing={4}
+                  direction="column"
+                  justifyContent="flex-start"
+                  alignItems="flex-start"
+                >
+                  <Grid item>
+                    <CardReasonsCard totalCardsDist={[matchData.home_total_cards, matchData.home_foul_cards, matchData.home_unprofessional_cards, matchData.home_dive_cards, matchData.home_other_cards]} />
+                  </Grid>
+                  <Grid item>
+                    {" "}
+                    <RedYellowCardCard numRed={matchData.home_red_cards} numYellow={matchData.home_yellow_cards} />{" "}
+                  </Grid>
+                  <Grid item>
+                    <FoulCard numFoul={matchData.home_fouls} numFoulPerCard={matchData.home_total_cards / matchData.home_fouls * 100} />
+                  </Grid>
+                </Grid>
+              </Box>
             </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
-  );
+            <Grid item>
+              <Timeline sx={{ width: 450 }}>
+                {timeline.map((item) => (
+                  <div
+                    key={
+                      item.event_id
+                    }
+                  >
+                    {createTimelineItem(
+                      item.event_text,
+                      item.event_side,
+                      item.event_time,
+                      item.event_type
+                    )}
+                  </div>
+                ))}
+              </Timeline>
+            </Grid>
+            <Grid item>
+              <Box sx={{ mr: 12 }}>
+                <Grid
+                  container
+                  spacing={4}
+                  direction="column"
+                  justifyContent="flex-start"
+                  alignItems="flex-start"
+                >
+                   <Grid item>
+                    <CardReasonsCard totalCardsDist={[matchData.away_total_cards, matchData.away_foul_cards, matchData.away_unprofessional_cards, matchData.away_dive_cards, matchData.away_other_cards]} />
+                  </Grid>
+                  <Grid item>
+                    {" "}
+                    <RedYellowCardCard numRed={matchData.away_red_cards} numYellow={matchData.away_yellow_cards} />{" "}
+                  </Grid>
+                  <Grid item>
+                    <FoulCard numFoul={matchData.away_fouls} numFoulPerCard={matchData.away_total_cards / matchData.away_fouls * 100} />
+                  </Grid>
+                </Grid>
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid
+            sx={{ mt: 0 }}
+            container
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography
+              color="black"
+              style={{ fontWeight: "bold", fontSize: 20 }}
+            >
+              Match Ends
+            </Typography>
+          </Grid>
+  
+          <Grid>
+            <Card
+              sx={{ height: 200, width: 800, mt: 4 }}
+              style={{ backgroundColor: "#DAD2BC" }}
+            >
+              {renderRate()}
+            </Card>
+          </Grid>
+        </Grid>
+      );
+    } else {
+      return (<h1>Loading...</h1>);
+    }
+  }
+
+  
 }
--- server/api/matchController.js
+++ server/api/matchController.js
@@ -1,10 +1,9 @@
 import db from "../db.js"
-import dotenv from "dotenv"
 
 export default class matchController{
     static async getMatchDataById(req, res, next){
         try {
-          const result = await db.query('SELECT * FROM wingman.matches m, wingman.referees r, wingman.matchtimelines tl, wingman.teams t WHERE m.match_id = $1 AND  t.teamid = m.home_id AND t.teamid = m.away_id AND m.referee_id = r.id', [req.params.id])
+          const result = await db.query('SELECT m.*, r.name, r.surname, r.age, r.currentseasonmatches,r.avatarurl, t.teamname as home_teamname, t.teamlogo as home_logo, t1.teamname as away_teamname, t1.teamlogo as away_logo FROM wingman.matches m, wingman.referees r, wingman.teams t, wingman.teams t1 WHERE m.match_id = $1 AND  t.teamid = m.home_id AND t1.teamid = m.away_id AND m.referee_id = r.id', [req.params.id])
           if(result.rows.length == 0)
           {
             throw {
@@ -13,11 +12,13 @@ export default class matchController{
               error: new Error()
             };
           }
+          const timeline = await db.query('SELECT * FROM wingman.matchtimelines WHERE match_id = $1', [req.params.id])
           res.status(200).json({
-          data: result.rows[0]
+          data: result.rows[0],
+          timeline: timeline.rows
           })
         } catch (err) {
-          console.log(`Error when getting one referee ${err}`)
+          console.log(`Error when getting one match ${err}`)
           if(err.code == 1)
           {
             res.status(404).json({detail:err.detail, data:[]})
