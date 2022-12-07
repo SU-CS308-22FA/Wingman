5f408b2e57bc424268f975523f84f66474c9c556
Berk Sezer <110382789+berksezer@users.noreply.github.com>
2022-12-07 22:57:04 +0300

    Organized the fixture page to look more structured

--- client/src/components/Fixtures.jsx
+++ client/src/components/Fixtures.jsx
@@ -1,157 +1,189 @@
 import { Grid } from "@mui/material";
-import { Card, CardContent, CardHeader, Divider } from '@mui/material';
+import { Card, CardContent, CardHeader, Divider, Avatar, Pagination, Stack } from "@mui/material";
 import { Navigate } from "react-router-dom";
-import CardActions from '@mui/material/CardActions';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import CardActions from "@mui/material/CardActions";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
-import matchCard from "../components/matchCard"
+import matchCard from "../components/matchCard";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useContext } from "react";
 import UserFinder from "../apis/UserFinder";
-
-
 const FixturesComponent = ({}) => {
-
-  const  [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true);
   const [pageSize, setPageSize] = useState(10);
-  const  [matches, setMacthes] = useState([]);
-
-  useEffect( () => {
-        const fetcData = async () => {
-            try {
-            const response = await UserFinder.get("/fixture");
-            console.log("res:",response);
-            setMacthes(response.data.data.users)
-            } catch (err) {}
-        };
-
-        fetcData();
-    }, []);
+  const [matches, setMacthes] = useState(null);
   const navigate = useNavigate();
 
   const handleAssign = (id) => {
     navigate(`/assign/${id}`);
   };
+  console.log(11111);
+  useEffect(() => {
+    console.log(222222);
+    const fetcData = async () => {
+      try {
+        const response = await UserFinder.get("/fixture");
+        console.log("res:", response);
+        setMacthes(response.data.data.users);
+      } catch (err) {
+        console.log("zoort");
+      }
+    };
 
-    return(
-    <center> 
-    
-    <div className="col-md-4 animated fadeIn">
-    
-    <div className="card"
-    onClick={() => console.log('CLICK')}>
-                
-                <div className="card-body">
-                <Grid container spacing={2}
-                justifyContent="center"
-                alignItems="center"
-                >
-                
-                <Grid item xs={4} >
-                  <div className="avatar">
-                    <img
-                      src={"https://i.hizliresim.com/t6q9rs6.png" }
-                      alt=""
-                      height="66" width="50"
-                    />
-                  </div>
-                  </Grid>
-                  <Grid item xs={1} >
-                    <p>3</p>
-                  </Grid>
-                  <Grid item xs={2} >
-                    <p>-</p>
-                  </Grid>
-                  <Grid item xs={1} >
-                    <p>3</p>
-                  </Grid>
-                  <Grid item xs={4} >
-                  <div className="avatar">
-                    <img
-                      src={"https://i.hizliresim.com/t6q9rs6.png"}
-                      
-                      alt=""
-                      height="66" width="50"
-                    />
-                  </div>
-                  </Grid>
-                  </Grid>
-                  </div>
-                  <h5 className="card-title">
-                    alaa
-                  </h5>
-                  </div>
-                  <Card sx={{ padding: 3, justifyContent: 'center' }} >
-                  <div className="card-body">
-                  <Grid container spacing={2}
-                justifyContent="center"
-                alignItems="center"
-                >
-                
-                <Grid item xs={4} >
-                  <div className="avatar">
-                    <img
-                      src={"https://i.hizliresim.com/t6q9rs6.png" }
-                      alt=""
-                      height="66" width="50"
-                    />
-                  </div>
-                  </Grid>
-                  <Grid item xs={1} >
-                    <p>3</p>
-                  </Grid>
-                  <Grid item xs={2} >
-                    <p>-</p>
-                  </Grid>
-                  <Grid item xs={1} >
-                    <p>3</p>
-                  </Grid>
-                  <Grid item xs={4} >
-                  <div className="avatar">
-                    <img
-                      src={"https://i.hizliresim.com/t6q9rs6.png"}
-                      
-                      alt=""
-                      height="66" width="50"
-                    />
-                  </div>
-                  </Grid>
-                  
-                  <CardContent>
-                    <Grid container spacing={2}
-                justifyContent="center"
-                alignItems="center">
-          
+    fetcData();
+  }, []);
 
-                    <Grid item xs = {2}>
-                      <p>{matches[0].hometeamname}</p>
-                    </Grid>
-                    <Grid item xs={8}>
-                      <Typography gutterBottom variant="h7" component="div">
-                      11.02.22
-                    </Typography></Grid>
-                    <Grid item xs = {2}>
-                      <p>{matches[0].awayteamname}</p>
-                    </Grid>
-                    </Grid>
-                  
-                
-                  </CardContent>
-                  <CardActions>
-                    
-                    <Button size="medium" onClick={() => handleAssign(1)}>Assign Referee</Button>
-                  </CardActions></Grid>
-                    </div>
-                    
-                  </Card>
-                  
-                  </div>
+  return (
+    <center>
+      <Stack alignItems="center">
+      <Typography>Week: {1}</Typography>
+      <Pagination count={16} page={1}  />
+      </Stack>
+      
+      <Card sx={{ height: 200, width: 1500, mt: 4 }}> 
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Avatar
+                  alt=""
+                  src={matches && matches[0].hometeamlogo}
+                  sx={{ width: 60, height: 80, mt: 1 }}/>
+                  <Typography>
+                    Fener
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
 
-    </center> 
-    )
-}
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Grid item xs container direction="row" spacing={2}justifyContent="space-around"
+                    alignItems="center">
+                    <Grid item>
+                    <Typography sx = {{mt: 5}} style={{ fontSize: 30 }}>
+                      {matches && matches[0].home_score} 
+                    </Typography>
+                    </Grid>
+                    <Grid item>
+                    <Typography sx = {{mt: 5}} style={{ fontSize: 30 }}>
+                      - 
+                    </Typography>
+                    </Grid>
+                    <Grid item>
+                    <Typography sx = {{mt: 5}} style={{ fontSize: 30 }}>
+                      {matches && matches[0].away_score} 
+                    </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item>
+                    <Typography>
+                      22.12.22
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Button size="medium" onClick={() => handleAssign(1)}>
+                    Assign Referee
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Avatar
+                  alt=""
+                  src={matches && matches[0].hometeamlogo}
+                  sx={{ width: 60, height: 80, mt: 1 }}/>
+                  <Typography >
+                    Fener
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+
+      <Card sx={{ height: 200, width: 1500, mt: 4 }}> 
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Avatar
+                  alt=""
+                  src={matches && matches[0].hometeamlogo}
+                  sx={{ width: 60, height: 80, mt: 1 }}/>
+                  <Typography>
+                    Fener
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Grid item xs container direction="row" spacing={2}justifyContent="space-around"
+                    alignItems="center">
+                    <Grid item>
+                    <Typography sx = {{mt: 5}} style={{ fontSize: 30 }}>
+                      {matches && matches[0].home_score} 
+                    </Typography>
+                    </Grid>
+                    <Grid item>
+                    <Typography sx = {{mt: 5}} style={{ fontSize: 30 }}>
+                      - 
+                    </Typography>
+                    </Grid>
+                    <Grid item>
+                    <Typography sx = {{mt: 5}} style={{ fontSize: 30 }}>
+                      {matches && matches[0].away_score} 
+                    </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item>
+                    <Typography>
+                      22.12.22
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Button size="medium" onClick={() => handleAssign(1)}>
+                    Assign Referee
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Avatar
+                  alt=""
+                  src={matches && matches[0].hometeamlogo}
+                  sx={{ width: 60, height: 80, mt: 1 }}/>
+                  <Typography >
+                    Fener
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+
+      
+    </center>
+  );
+};
 
 export default FixturesComponent;
