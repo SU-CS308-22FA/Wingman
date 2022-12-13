16930d546666f8599924daa5cc20cc85ae1f098a
Ege Demirci <52569205+egedemirci@users.noreply.github.com>
2022-12-13 20:21:52 +0300

    Remove isVlidPass function from CreateReferee code
    This function is no longer needed because its functionality has
    been in corporated into the main code logic. Removing it simplifies
    the code and reduces the overall size of the file.

--- client/src/components/CreateReferee.component.jsx
+++ client/src/components/CreateReferee.component.jsx
@@ -21,17 +21,17 @@ const CreateReferee = () => {
   const [inputs, setInputs] = useState({
     name: "",
     surname: "",
-    totalmatches: 0,
-    totalyellowcards: 0,
-    totalredcards: 0,
-    age: 0,
-    currentseasonmatches: 0,
-    totalfoulspg : 0,
-    currentyel: 0,
-    currentred: 0,
-    currentfoulspg: 0,
-    totalpenpg:0,
-    currentpenpg: 0,
+    totalmatches: NaN,
+    totalyellowcards: NaN,
+    totalredcards: NaN,
+    age: NaN,
+    currentseasonmatches: NaN,
+    totalfoulspg : NaN,
+    currentyel: NaN,
+    currentred: NaN,
+    currentfoulspg: NaN,
+    totalpenpg:NaN,
+    currentpenpg: NaN,
 
   });  
   
@@ -56,40 +56,42 @@ const CreateReferee = () => {
         return /^-?\d+$/.test(value);
       }
 
-  function isValidPass(pass) {
-    if(pass.length >= 8)
-    {
-      return true;
-    } else{
-      return false;
-    }
-  };
    const onChange = e =>
     setInputs({ ...inputs, [e.target.name]: e.target.value });
 
   const onSubmitForm = async e => {
     e.preventDefault();
     try {
-      if(!isAlphabetical(name)){
+      if(name.length == 0 || surname.length == 0){
+        throw{fmessage: "Name or surname cannot be empty. Please check the both fields."} }   
+      else if(!isAlphabetical(name)){
         throw{fmessage: "Name contains non-alphabetical characters. Please check the name field."} }    
       else if(!isAlphabetical(surname)){
         throw{fmessage: "Surname contains non-alphabetical characters. Please check the name field."}}
+      else if(age.length == null || totalmatches.length== null || totalyellowcards.length== null|| totalredcards.length== null || currentseasonmatches.length== null||
+          currentyel.length== null||currentred.length== null || age.length == 0 || totalmatches.length== 0 || totalyellowcards.length== 0|| totalredcards.length== 0 || currentseasonmatches.length== 0||
+          currentyel.length== 0||currentred.length== 0){
+              throw{fmessage: "Statistics specifying net numbers must be filled. Please check if there any empty field."}}
       else if(!isInteger(age) || !isInteger(totalmatches) || !isInteger(totalredcards) || !isInteger(totalyellowcards) ||
               !isInteger(currentseasonmatches)|| !isInteger(currentyel)||  !isInteger(currentred)){
-        throw{fmessage: "Total exact amounts cannot be empty and all of them must be integers."}}
+        throw{fmessage: "Statistics specifying net numbers must be integers."}}     
+      else if(totalmatches.length != 0 && currentseasonmatches.length != 0 && parseInt(totalmatches)<parseInt(currentseasonmatches)){
+          throw{fmessage: "Total number of matches cannot be smaller than current season matches. Please check the both fields."}}
+      else if(totalredcards.length != 0 && currentred.length != 0 && parseInt(totalredcards)< parseInt(currentred)){
+          throw{fmessage: "Total number of red cards cannot be smaller than current season red cards. Please check the both fields."}
+        }
+      else if(totalyellowcards.length != 0 && currentyel.length != 0 && parseInt(totalyellowcards)<parseInt(currentyel)){ 
+            throw{fmessage: "Total number of yellow cards cannot be smaller than current season yellow cards. Please check the both fields."}}
+      else if( totalfoulspg.length== null|| currentfoulspg.length== null ||totalpenpg.length== null||currentpenpg.length== null
+       || totalfoulspg.length== 0 || currentfoulspg.length== 0 ||totalpenpg.length== 0 ||currentpenpg.length== 0){
+            throw{fmessage: "Per game statistics must be filled. Please check if there any empty field."}}
       else if(!isNumeric(totalfoulspg)|| !isNumeric(currentfoulspg) || !isNumeric(totalpenpg)|| !isNumeric(currentpenpg)){
-        throw{fmessage: "Statistical values cannot include non numeric values."}}
-      else if(totalmatches.length != 0 && currentseasonmatches.length != 0 && totalmatches<currentseasonmatches){
-                throw{fmessage: "Total number of matches cannot be smaller than current season matches. Please check the both fields."}}
-      else if(totalredcards.length != 0 && currentred.length != 0 && totalredcards<currentred){
-        console.log(currentred);
-
-                  throw{fmessage: "Total number of red cards cannot be smaller than current season red cards. Please check the both fields."}}
-      else if(totalyellowcards.length != 0 && currentyel.length != 0 && totalyellowcards<currentyel){
-                    throw{fmessage: "Total number of yellow cards cannot be smaller than current season yellow cards. Please check the both fields."}}
-      else if(name.length == 0 || surname.length == 0 || age.length == 0 || totalmatches== 0 || totalyellowcards== 0|| totalredcards== 0 || currentseasonmatches== 0||
-                            totalfoulspg== 0||currentyel== 0||currentfoulspg== 0||currentred== 0||totalpenpg== 0||currentpenpg== 0){
-          throw{fmessage: "All fields must be filled. Please check if there any empty field."}}
+        throw{fmessage: "Per game statistics cannot include non numeric values."}}
+      else if(name.length == 0 || surname.length == 0 || age.length == null || totalmatches.length== 0 || totalyellowcards.length== 0|| totalredcards.length== 0 || currentseasonmatches.length== 0||
+                            totalfoulspg.length== null||currentyel.length== null||currentfoulspg.length== 0||currentred.length== 0||totalpenpg.length== 0||currentpenpg.length== 0
+      ||  age.length == 0 || totalmatches.length== 0 || totalyellowcards.length== 0|| totalredcards.length== 0 || currentseasonmatches.length== 0||
+      totalfoulspg.length== 0||currentyel.length== 0||currentfoulspg.length== 0||currentred.length== 0||totalpenpg.length== 0||currentpenpg.length== 0){
+            throw{fmessage: "All fields must be filled. Please check if there any empty field."}}
 
 
 
@@ -110,6 +112,8 @@ const CreateReferee = () => {
         currentfoulspg: currentfoulspg,
         totalpenpg : totalpenpg,
         totalyelpg: totalyellowcards/totalmatches,
+        currentyelpg: currentyel/currentseasonmatches,
+        currentredpg: currentred/ currentseasonmatches,
         totalredpg: totalredcards/totalmatches,
         currentpenpg:currentpenpg,
         avatarurl: "https://img.freepik.com/premium-vector/referee-avatar-icon-flat-color-style_755164-489.jpg?w=2000",
@@ -129,18 +133,22 @@ const CreateReferee = () => {
             currentyel: response.data.data.currentyel,
             currentred: response.data.data.currentred,
             currentfoulspg: response.data.data.currentfoulspg,
+            currentyelpg: response.data.data.currentyelpg,
+            currentredpg : response.data.data.currentredpg,
             totalpenpg : response.data.data.totalpenpg,
             totalyelpg: response.data.data.totalyelpg,
             totalredpg: response.data.data.totalredpg,
             currentpenpg:response.data.data.currentpenpg,
             avatarurl:response.data.data.avatarurl
           } 
+
+        
           setReferee(val)  
-          navigate("/refereelist")}
+          navigate(`/referee/${val.id}`)}
     } catch (err) {
         if(err.fmessage)
             setError(err.fmessage)
-        else
+        else            
             setError("There was an unknown problem.")
         console.error('onSubmit form error: ', err);
     }
@@ -168,7 +176,7 @@ const CreateReferee = () => {
 
           <Box component="form" noValidate onSubmit={onSubmitForm} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={4}>
                 <TextField
                   autoComplete="given-name"
                   name="name"
@@ -180,7 +188,7 @@ const CreateReferee = () => {
                   autoFocus
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={4}>
                 <TextField
                   required
                   fullWidth
@@ -191,52 +199,83 @@ const CreateReferee = () => {
                   onChange={e => onChange(e)}
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="totalmatches"
-                  label="Total Matches"
-                  name="totalmatches"
-                  autoComplete={currentseasonmatches}
-                  onChange={e => onChange(e)}
-                />
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={4}>
                 <TextField
                   required
                   fullWidth
                   id="age"
-                  label="Age"
+                  label="Age (Net Number)"
                   name="age"
                   autoComplete="age"
                   onChange={e => onChange(e)}
                   
                 />
               </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="totalmatches"
+                  label="Total Matches (Net Number)"
+                  name="totalmatches"
+                  autoComplete={currentseasonmatches}
+                  onChange={e => onChange(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentseasonmatches"
+                 label="Current Season Matches (Net Number)"
+                  name="currentseasonmatches"
+                 autoComplete="currentseasonmatches"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
 
               <Grid item xs={12} sm={4}>
                 <TextField
                   required
                   fullWidth
                   id="totalredcards"
-                  label="Total Red Cards"
+                  label="Total Seasons Red Cards (Net Number)"
                   name="totalredcards"
                   autoComplete="totalredcards"
                   onChange={e => onChange(e)}
                 />
               </Grid>
-    
+              <Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentred"
+                 label="Current Season Red Cards (Net Number)"
+                  name="currentred"
+                 autoComplete="currentred"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
 
 <Grid item xs={12} sm={4}>
                <TextField
                  required
                  fullWidth
                   id="totalyellowcards"
-                 label="Total Yellow Cards"
+                 label="Total Seasons Yellow Cards (Net Number)"
                   name="totalyellowcards"
                  autoComplete="totalyellowcards"
+                 onChange={e => onChange(e)}
+               />
+             </Grid>
+             <Grid item xs={12} sm={4}>
+               <TextField
+                 required
+                 fullWidth
+                  id="currentyel"
+                 label="Current Season Yellow Cards (Net Number)"
+                  name="currentyel"
+                 autoComplete="currentyel"
                  onChange={e => onChange(e)}
                />
              </Grid>
@@ -245,74 +284,44 @@ const CreateReferee = () => {
                <TextField
                  required
                  fullWidth
-                  id="currentseasonmatches"
-                 label="Current Season Matches"
-                  name="currentseasonmatches"
-                 autoComplete="currentseasonmatches"
-                 onChange={e => onChange(e)}
-               />
-             </Grid>
-<Grid item xs={12} sm={4}>
-               <TextField
-                 required
-                 fullWidth
                   id="totalfoulspg"
-                 label="Total Fouls Per Game"
+                 label="Total Seasons Fouls Per Game"
                   name="totalfoulspg"
                  autoComplete="totalfoulspg"
                  onChange={e => onChange(e)}
                />
              </Grid>
-<Grid item xs={12} sm={4}>
-               <TextField
-                 required
-                 fullWidth
-                  id="currentyel"
-                 label="Current Season Yellow Cards"
-                  name="currentyel"
-                 autoComplete="currentyel"
-                 onChange={e => onChange(e)}
-               />
-             </Grid>
+
 <Grid item xs={12} sm={4}>
                <TextField
                  required
                  fullWidth
                   id="currentfoulspg"
-                 label="Current Fouls Per Game"
+                 label="Current Season Fouls Per Game"
                   name="currentfoulspg"
                  autoComplete="currentfoulspg"
                  onChange={e => onChange(e)}
                />
              </Grid>
-<Grid item xs={12} sm={4}>
-               <TextField
-                 required
-                 fullWidth
-                  id="currentred"
-                 label="Current Season Red Cards"
-                  name="currentred"
-                 autoComplete="currentred"
-                 onChange={e => onChange(e)}
-               />
-             </Grid>
+
+
 <Grid item xs={12} sm={4}>
                <TextField
                  required
                  fullWidth
                   id="totalpenpg"
-                 label="Total Penalty Per Game"
+                 label="Total Seasons Penalty Per Game"
                   name="totalpenpg"
                  autoComplete="totalpenpg"
                  onChange={e => onChange(e)}
                />
              </Grid>
-<Grid item xs={12} sm={4}>
+      <Grid item xs={12} sm={4}>
                <TextField
                  required
                  fullWidth
                   id="currentpenpg"
-                 label="Current Penalty Per Game"
+                 label="Current Season Penalty Per Game"
                   name="currentpenpg"
                  autoComplete="currentpenpg"
                  onChange={e => onChange(e)}
@@ -320,6 +329,7 @@ const CreateReferee = () => {
              </Grid>
 
             </Grid>
+            
             <Box m={1} pt={0}> </Box>
             {error &&<Alert variant="filled" severity="error"> {error} </Alert>}
             <Button
