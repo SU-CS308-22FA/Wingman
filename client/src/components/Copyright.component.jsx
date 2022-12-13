d01ec303eb19965ab62e0bb1515140941441ed8e
Ege Demirci <52569205+egedemirci@users.noreply.github.com>
2022-12-13 21:33:03 +0300

    Revert "Update to copyright component and referee list"
    This reverts commit c8d6d94e5e309330c01176959af106288c537eea.

--- client/src/components/Copyright.component.jsx
+++ client/src/components/Copyright.component.jsx
@@ -5,8 +5,8 @@ import Typography from '@mui/material/Typography';
 function Copyright(props) {
     return (
       <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Wingman © '}
-        <Link color="inherit" href="https://wingman-team29.herokuapp.com/">
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
           Wingman
         </Link>{' '}
         {new Date().getFullYear()}
