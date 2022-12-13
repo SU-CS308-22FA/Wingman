6e1387682152b14d0825dad71aebd7ec7b96a28f
Berk Sezer <110382789+berksezer@users.noreply.github.com>
2022-12-13 16:57:17 +0300

    Add fixture to app bar
    The app bar did not have the fixture tab.
    
    User needed to go to the specific URL in order to reach the fixture.
    
    Now with the added fixture tab the user can access the fixture page from the app bar.
    
    By putting the fixture into the app bar the user will navigate faster between pages.

--- client/src/components/LoggedInAppBar.jsx
+++ client/src/components/LoggedInAppBar.jsx
@@ -35,6 +35,9 @@ function ResponsiveAppBar() {
       const onTeamsClicked = (e) => {
         navigate("/teams");
       };
+      const onFixtureClicked = (e) => {
+        navigate("/fixture/1");
+      };
 const tiers = [
     {
         pageName:"Profile",
@@ -52,6 +55,10 @@ const tiers = [
         pageName:"Teams",
         onClick: onTeamsClicked
     },
+    {
+      pageName:"Fixture",
+      onClick: onFixtureClicked
+  },
 
 
   ];
